d65a90eedd05317e8ea4527b9e23c092c6848d83
lzyutada <38025255+lzyutada@users.noreply.github.com>
2024-02-18 02:29:58 +0000

    using module for logger

--- MyTest/Examples/odbc/mongodb/ex.py
+++ MyTest/Examples/odbc/mongodb/ex.py
@@ -9,42 +9,8 @@ class MyMongoDbInfo :
     username="user"
     pwd="user"
 
-def get_logger():
-    '''
-    '''
-
-    import os
-    import datetime
-    from datetime import datetime
-    import logging
-    from logging.handlers import TimedRotatingFileHandler
-        
-    # 获取当前日期
-    current_date = datetime.now().strftime('%Y%m%d')
-    log_folder = 'log'
-
-    # 如果log文件夹不存在，则创建
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
-    log_file = os.path.join(log_folder, f'{current_date}.log')
-
-    # 创建日志记录器
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    # 创建 TimedRotatingFileHandler
-    file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=0)
-    file_handler.setLevel(logging.DEBUG)
-
-    # 创建日志格式
-    formatter = logging.Formatter('[%(asctime)s][%(levelname)s]: %(message)s')
-    file_handler.setFormatter(formatter)
-
-    # 添加处理器到日志记录器
-    logger.addHandler(file_handler)
-    return logger
-
-logger = get_logger()
+import loghelper.loghelper
+logger = loghelper.loghelper.get_logger()
 
 def import_module(): 
     """
--- MyTest/main.py
+++ MyTest/main.py
@@ -14,42 +14,8 @@
 """
 '''the following examples refers to: 「页面」Python基础语法 | 菜鸟教程  https://www.runoob.com/python/python-basic-syntax.html'''
 
-def get_logger():
-    '''
-    '''
-
-    import os
-    import datetime
-    from datetime import datetime
-    import logging
-    from logging.handlers import TimedRotatingFileHandler
-        
-    # 获取当前日期
-    current_date = datetime.now().strftime('%Y%m%d')
-    log_folder = 'log'
-
-    # 如果log文件夹不存在，则创建
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
-    log_file = os.path.join(log_folder, f'{current_date}.log')
-
-    # 创建日志记录器
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    # 创建 TimedRotatingFileHandler
-    file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=0)
-    file_handler.setLevel(logging.DEBUG)
-
-    # 创建日志格式
-    formatter = logging.Formatter('[%(asctime)s][%(levelname)s]: %(message)s')
-    file_handler.setFormatter(formatter)
-
-    # 添加处理器到日志记录器
-    logger.addHandler(file_handler)
-    return logger
-
-logger = get_logger()
+import loghelper.loghelper
+logger = loghelper.loghelper.get_logger()
 
 from flask import Flask
 
@@ -59,14 +25,12 @@ app = Flask(__name__)
 def run() :
     respDes = ""; msgDes = ""
     msgList = __odbc_mongodb_ex()
-    # print(F"msgList={msgList}")
-    # # for letter in 'Python':
     for msg in msgList:
-        # logger.debug(F"msg={msg}")
+        logger.debug(F"msg={msg}")
         msgDes = F"""{msgDes}<div>{msg}</div>"""
     respDes = F'<div style="display: flex; flex-direction: column;max-width: 600px; margin: 0 auto;">{msgDes}</div>'
     
-    # logger.debug(F"respDes={respDes}\r\nmsgDes={msgDes}")
+    logger.debug(F"respDes={respDes}\r\nmsgDes={msgDes}")
     return respDes
 
 if __name__=="__main__":
